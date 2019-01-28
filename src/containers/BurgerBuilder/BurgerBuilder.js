9328f881a14d75242e9b2820e964f73346e273dd
Sorin <kidoatguitar@gmail.com>
2019-01-28 23:32:51 +0200

    Adding the Order Button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,7 @@ class BurgerBuilder extends Component {
         salad: 0,
         bacon: 0,
         cheese: 0,
-        meat: 2
+        meat: 0
       },
       totalPrice: 4,
       purchasable: false,
@@ -28,27 +28,59 @@ class BurgerBuilder extends Component {
     }
   }
 
+  updatePurchaseState = (ingredients) => {
+    const sum = Object.keys(ingredients).reduce(
+      (total, ingredient) => {
+        return total + ingredients[ingredient]
+      }, 0);
+
+    this.setState({ purchasable: sum > 0 });
+  }
+
   addIngredientHandler = (type) => {
-    const OldCount = this.state.ingredients[type];
-    const updatedCount = OldCount + 1;
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
     const updatedIngredients = { ... this.state.ingredients }
     updatedIngredients[type] = updatedCount;
+
     const priceAddition = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
   }
 
-  removeIngredientHandler = () => {
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = { ... this.state.ingredients }
+    updatedIngredients[type] = updatedCount;
 
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
   }
 
   render() {
+    const disabledInfo = Object.entries(this.state.ingredients).reduce((result, [key, value]) => {
+      result[key] = (value <= 0);
+      return result
+    }, {});
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
-          ingredientAdded={this.addIngredientHandler} />
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+          purchasable={this.state.purchasable} />
       </Aux>
     );
   }
